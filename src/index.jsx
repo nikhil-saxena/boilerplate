c50afec9bf78bd8d343dfea1311037be829721c2
Nikhil <saxena.nik2@gmail.com>
2019-04-23 17:03:21 +0530

    feat: enable hmr

--- src/index.jsx
+++ src/index.jsx
@@ -19,3 +19,8 @@ const Index = () => {
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
+
+// Needed for Hot Module Replacement
+if (typeof module.hot !== "undefined") {
+  module.hot.accept(); // eslint-disable-line no-undef
+}
